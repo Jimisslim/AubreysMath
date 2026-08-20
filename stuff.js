8acedbafc66b16fc11daddc3d698c01672f2a569
Jimisslim <129909191+Jimisslim@users.noreply.github.com>
2026-08-19 23:57:46 -0700

    removal

--- stuff.js
+++ stuff.js
@@ -4,7 +4,6 @@ const CONFIG = {
 
 const CATALOG = [
   { id: 1, type: "tv", title: "Jojo's Bizzare Adventure (TV)", year: 2012, genre: "Anime", file: "Shows/JoJos/JoJo - S01E01-fixed.mp4", poster: "" },
-  { id: 2, type: "movie", title: "Lego Batman", year: 2017, genre: "Lego",  file: "movies/low-tide.mp4",  poster: "" },
 ];
 
 
